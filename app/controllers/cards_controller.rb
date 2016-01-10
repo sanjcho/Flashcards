4d2edd478ceee8ef49dd39a7cb8aa8ff1aecb4d8
Aleksandr <sanjcho@gmail.com>
2016-01-10 23:14:39 +0500

    Form render, new action and validates ready

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,35 @@
 class CardsController < ApplicationController
+  
+
   def show
     @cards = Card.all
   end
+
+  def new
+  	@card = Card.new
+  end
+
+  def create
+  	@card = Card.create(card_params)
+  	@card.save
+  	redirect_to cards_path
+  end
+
+  def edit
+  end
+
+  def update
+  	@card = Card.update(card_params)
+  	@card.save
+  	redirect_to cards_path
+  end
+
+  def destroy
+
+  end
+
+  def card_params
+  	params.require(:card).permit(:original_text, :translated_text)
+  end
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,7 @@
 class Card < ActiveRecord::Base
+validates :original_text, :translated_text, presence: true, uniqueness: { case_sensitive: false }
+validates :review_date, presence: true
+  before_validation do
+    self.review_date = DateTime.now.days_since(3)
+  end
 end
