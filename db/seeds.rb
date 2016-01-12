825027ef9fc5f12f51062274c87d53e97ed3017b
Aleksandr <sanjcho@gmail.com>
2016-01-12 21:51:37 +0500

    style correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
 page = Nokogiri::HTML(open("http://www.homeenglish.ru/250Popular1.htm"))
-a=14
-100.times do 
-  orig=page.css('td')[a].text 
-  transl=page.css('td')[a+1].text
-  a=a+3
-  Card.create(original_text: orig, translated_text: transl )
+a = 14
+100.times do
+  orig = page.css("td")[a].text
+  transl = page.css("td")[a + 1].text
+  a += 	3
+  Card.create(original_text: orig, translated_text: transl)
 end
