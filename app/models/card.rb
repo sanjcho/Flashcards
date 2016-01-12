68a9d9ebe653d4a4e1c1aca456f8226eecce365d
Aleksandr <sanjcho@gmail.com>
2016-01-12 12:01:14 +0500

    Full worked Validate

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,15 +1,17 @@
-class TextComparisonValidator < ActiveModel::Validator
-  def validate(record)
-    if record.original_text.downcase == record.translated_text.downcase
-      record.errors.add( :original_text,:must_be_diff_w_transl)
-    end
-  end
-end
 class Card < ActiveRecord::Base
 validates :original_text, :translated_text, presence: true, uniqueness: { case_sensitive: false }
 validates :review_date, presence: true
-validates_with TextComparisonValidator
-  before_validation do
+validate :must_not_be_equal
+  before_validation(on: :create) do
     self.review_date = DateTime.now.days_since(3)
   end
+  
+  def must_not_be_equal
+    if self.original_text.downcase == self.translated_text.downcase
+      self.errors.add( :original_text,:must_be_diff_w_transl)
+    end
+  end
+
+
+
 end
