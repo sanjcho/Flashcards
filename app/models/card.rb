df92620847f84789e1dedcf9fe02fd39c132d3ca
Aleksandr <sanjcho@gmail.com>
2016-01-14 17:57:05 +0500

    scope :expired work ok

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,10 +2,13 @@ class Card < ActiveRecord::Base
 validates :original_text, :translated_text, presence: true, uniqueness: { case_sensitive: false }
 validates :review_date, presence: true
 validate :must_not_be_equal
+scope :expired, ->{ where('review_date <= ?', DateTime.now) }
   before_validation(on: :create) do
     self.review_date = DateTime.now.days_since(3)
   end
   
+
+
   def must_not_be_equal
     if self.original_text.downcase == self.translated_text.downcase
       self.errors.add( :original_text,:must_be_diff_w_transl)
