424c3c0f97dce18fddc95ad8755b55e74b133322
Aleksandr <sanjcho@gmail.com>
2016-01-18 12:11:20 +0500

    some small corrections, style improve,add validation tests

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,5 +1,5 @@
 module Helpers
-  def card_new (orig, transl) #new card without .save
+  def card_new(orig, transl) # new card without .save
     Card.new(original_text: orig, translated_text: transl, review_date: DateTime.now)
   end
 end
--- spec/models/card_test_spec.rb
+++ spec/models/card_test_spec.rb
@@ -7,25 +7,43 @@ end
 
 RSpec.describe Card,:type => :model do
   context "card methods tests" do
-    it "Card object with the same original and translated text creation" do
-      c=card_new("mom", "Mom")
-      expect(c.save).to be false
+    it "validate :must_not_be_equal test" do
+      card = card_new("mom", "Mom")
+      expect(card).to be_invalid
     end
 
-    it "Actualising review date without DB update" do
-    	expect(card_new("mom", "мама").review_actualize.to_i).to eq DateTime.now.days_since(3).to_i
+    it "validates :review_date, presence: true test" do
+      card = card_new("mom", "Mom")
+      card.review_date = nil
+      expect(card).to be_invalid
     end
 
-    it "Original text compared with the some inout text" do
-      c=card_new("mom", "мама")
-      expect(c.original_text_equal_to?("mom")).to be true
+    it "validates :original_text and :translated_text presence test" do
+      expect(card_new("", "mom")).to be_invalid
+      expect(card_new("Daddy", "")).to be_invalid
     end
 
-    it "Updating review_date with direct DB correction" do
-      c=card_new("mom", "мама")
-      c.save
-      c.update_review_date!
-      expect(Card.find(c.id).review_date.to_s).to eq Date.today.days_since(3).to_s
+    it "validates :original_text and :translated_text uniqueness test" do
+      card = card_new("mom", "мама")
+      card.save
+      expect(card_new("mom", "мамочка")).to be_invalid
+      expect(card_new("mommy", "мама")).to be_invalid
+    end
+
+    it ".review_actualize test" do
+      expect(card_new("mom", "мама").review_actualize.to_i).to eq DateTime.now.days_since(3).to_i
+    end
+
+    it ".original_text_equal_to? test" do
+      card = card_new("mom", "мама")
+      expect(card.original_text_equal_to?("mom")).to be true
+    end
+
+    it ".update_review_date! test" do
+      card = card_new("mom", "мама")
+      card.save
+      card.update_review_date!
+      expect(Card.find(card.id).review_date.to_s).to eq Date.today.days_since(3).to_s
     end
   end
 end
