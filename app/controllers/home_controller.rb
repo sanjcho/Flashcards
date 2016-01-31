7bd934971a0c98d75298a4d850006950a9910220
Aleksandr <sanjcho@gmail.com>
2016-01-31 21:02:12 +0500

    refactor HomeController for a little

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :require_login
   def index
     if current_user
-      @user = current_user
-      @card = @user.cards.expired.random.first
+      @card = current_user.cards.expired.random.first
     end
   end
 end
