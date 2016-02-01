0fa4a4ecf0241e4b91bf595ade1b1178a6537cb9
Aleksandr Makarenko <sanjcho@gmail.com>
2016-02-01 17:53:31 +0500

    locks like pretty complete Deck controller, but no nested routes and views

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,19 +1,47 @@
 class DecksController < ApplicationController
+
+  before_action :set_user
+
   def index
+    @deck = @user.Deck.all
   end
 
   def new
+    @deck = @user.Deck.new
   end
 
   def create
+    @deck = Deck.new(deck_params)
+    if @deck.save
+      redirect to decks_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @deck = Deck.find(params[:id])
+
   end
 
   def update
+    @deck = Deck.find(params[:id])
+    if @deck.update(deck_params)
+      redirect_to decks_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @deck.find(params[:id])
+    @deck.destroy
+    redirect_to decks_path
   end
+
+  private
+    def deck_params
+      params.require(:deck).permit(:name, :id)
+    end
+
 end
