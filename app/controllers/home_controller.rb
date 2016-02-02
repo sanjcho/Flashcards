1c61b18974d01538627cb213f688f39d8fb01d43
Aleksandr Makarenko <sanjcho@gmail.com>
2016-02-02 17:55:32 +0500

    fixet bug in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
     if current_user
       if current_user.active_deck_id?
         @card = Deck.find(current_user.active_deck_id).cards.expired.random.first 
+        @deck = @card.deck if @card != nil
       else
         @card = current_user.cards.expired.random.first
       end
-      @deck = @card.deck
     end
   end
 end
