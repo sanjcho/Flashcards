35e0089f858e1fc8359430d92247657983bc8923
Aleksandr Makarenko <sanjcho@gmail.com>
2016-02-05 15:13:53 +0500

    remove some unneeded field in user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201145458) do
+ActiveRecord::Schema.define(version: 20160205090803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20160201145458) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "salt"
-    t.integer  "active_deck_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
