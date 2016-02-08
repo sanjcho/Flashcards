8cad0404ce0496acf8a32c5ae5abb51543fe8421
Aleksandr Makarenko <sanjcho@gmail.com>
2016-02-08 18:09:26 +0500

    add time to review_date field in cards, spec not pass, just for save

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208124956) do
+ActiveRecord::Schema.define(version: 20160208130042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160208124956) do
   create_table "cards", force: :cascade do |t|
     t.text     "original_text"
     t.text     "translated_text"
-    t.date     "review_date"
+    t.datetime "review_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "user_id"
