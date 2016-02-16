0d9b9b4425cc3d51ab12d00c0dfbcf233513efac
Aleksandr <sanjcho@gmail.com>
2016-02-16 17:36:52 +0500

    app controller made for locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
   before_action :set_locale
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
-  end
+
 
   def set_locale
     I18n.locale = if current_user
                  current_user.locale
-               elsif params[:locale]
-               	 params[:locale]
+               elsif params[:locale] == "en" || params[:locale] == "ru"
+               	 session[:locale] = params[:locale]
+               elsif session[:locale]
+                 session[:locale]
                	else
                  http_accept_language.compatible_language_from(I18n.available_locales)
                 end
